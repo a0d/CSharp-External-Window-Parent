1b9d738878688125122303fa6c1c90c1ebd7f038
BitByte <xgenwares@gmail.com>
2016-02-20 14:03:21 -0500

    Change an external window's handle in CSharp
    Easy way to set an external window's handle to a panel in a form. This
    produce's a "docking" type effect.

---
+++ ExternalWindowForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ExternalMDIChild
+{
+    partial class ExternalWindowForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FileMenuButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitMenuButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.ProgramsMenuButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.NotepadMenuButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.CalculatorMenuButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.PaintMenuButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.WindowContainer = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 513);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(825, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileMenuButton,
+            this.ProgramsMenuButton});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(825, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // FileMenuButton
+            // 
+            this.FileMenuButton.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ExitMenuButton});
+            this.FileMenuButton.Name = "FileMenuButton";
+            this.FileMenuButton.Size = new System.Drawing.Size(37, 20);
+            this.FileMenuButton.Text = "File";
+            // 
+            // ExitMenuButton
+            // 
+            this.ExitMenuButton.Name = "ExitMenuButton";
+            this.ExitMenuButton.Size = new System.Drawing.Size(92, 22);
+            this.ExitMenuButton.Text = "Exit";
+            this.ExitMenuButton.Click += new System.EventHandler(this.ExitMenuButton_Click);
+            // 
+            // ProgramsMenuButton
+            // 
+            this.ProgramsMenuButton.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NotepadMenuButton,
+            this.CalculatorMenuButton,
+            this.PaintMenuButton});
+            this.ProgramsMenuButton.Name = "ProgramsMenuButton";
+            this.ProgramsMenuButton.Size = new System.Drawing.Size(70, 20);
+            this.ProgramsMenuButton.Text = "Programs";
+            // 
+            // NotepadMenuButton
+            // 
+            this.NotepadMenuButton.Name = "NotepadMenuButton";
+            this.NotepadMenuButton.Size = new System.Drawing.Size(156, 22);
+            this.NotepadMenuButton.Text = "Notepad";
+            this.NotepadMenuButton.Click += new System.EventHandler(this.NotepadMenuButton_Click);
+            // 
+            // CalculatorMenuButton
+            // 
+            this.CalculatorMenuButton.Name = "CalculatorMenuButton";
+            this.CalculatorMenuButton.Size = new System.Drawing.Size(156, 22);
+            this.CalculatorMenuButton.Text = "Calculator";
+            this.CalculatorMenuButton.Click += new System.EventHandler(this.CalculatorMenuButton_Click);
+            // 
+            // PaintMenuButton
+            // 
+            this.PaintMenuButton.Name = "PaintMenuButton";
+            this.PaintMenuButton.Size = new System.Drawing.Size(152, 22);
+            this.PaintMenuButton.Text = "Paint";
+            this.PaintMenuButton.Click += new System.EventHandler(this.CommandLineMenuButton_Click);
+            // 
+            // WindowContainer
+            // 
+            this.WindowContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.WindowContainer.Location = new System.Drawing.Point(0, 24);
+            this.WindowContainer.Name = "WindowContainer";
+            this.WindowContainer.Size = new System.Drawing.Size(825, 489);
+            this.WindowContainer.TabIndex = 3;
+            // 
+            // ExternalWindowForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 535);
+            this.Controls.Add(this.WindowContainer);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ExternalWindowForm";
+            this.Text = "ExternalMDIChild";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FileMenuButton;
+        private System.Windows.Forms.ToolStripMenuItem ExitMenuButton;
+        private System.Windows.Forms.ToolStripMenuItem ProgramsMenuButton;
+        private System.Windows.Forms.ToolStripMenuItem NotepadMenuButton;
+        private System.Windows.Forms.Panel WindowContainer;
+        private System.Windows.Forms.ToolStripMenuItem CalculatorMenuButton;
+        private System.Windows.Forms.ToolStripMenuItem PaintMenuButton;
+    }
+}
+
---
+++ ExternalWindowForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Diagnostics;
+namespace ExternalMDIChild
+{
+    public partial class ExternalWindowForm : Form
+    {
+        public ExternalWindowForm()
+        {
+            InitializeComponent();
+        }
+        //External refernce to user32.dll to set the parent property of an external window
+
+        [DllImport("user32")]
+        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+
+        private void NotepadMenuButton_Click(object sender, EventArgs e)
+        {
+            StartWindowedProcess("notepad.exe");
+        }
+        private void StartWindowedProcess(String process_name)
+        {
+            //Declare a new process
+            var proc = new Process
+            {
+                //Define initial process info
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = process_name
+                }
+            };
+            //Start the process
+            proc.Start();
+            //Wait for the process to fully start so the MainWindowHandle can be determined
+            proc.WaitForInputIdle();
+            Thread.Sleep(500);
+            //Using the MainWindowHandle property, set the window's parent to the WindowContainer panel
+            SetParent(proc.MainWindowHandle, this.WindowContainer.Handle);
+        }
+
+        private void ExitMenuButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CalculatorMenuButton_Click(object sender, EventArgs e)
+        {
+            StartWindowedProcess("calc.exe");
+        }
+
+        private void CommandLineMenuButton_Click(object sender, EventArgs e)
+        {
+            StartWindowedProcess("mspaint.exe");
+        }
+    }
+}
